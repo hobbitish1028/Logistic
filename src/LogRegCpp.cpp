84b3229fab5a716c775ba1cc5a3c96c34ced627f
ChangWang <wangchan@umich.edu>
2019-11-22 12:12:31 -0500

    cahnge cpp

--- src/LogRegCpp.cpp
+++ src/LogRegCpp.cpp
@@ -92,7 +92,7 @@ List LogRegcpp(NumericMatrix X, NumericVector x, NumericVector y ,int maxit){
     }
     loss[i] = loss0;  // Loss function is the log likelihood
     
-    if( i>500 ){
+    if( i>3000 ){
       double mu = 0; 
       for(int j = i-10; j < i; j++){
         mu += loss[j]; 
