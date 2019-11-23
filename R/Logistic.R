e924353feb718bbb9ec18d72c743748ef4f27078
ChangWang <wangchan@umich.edu>
2019-11-22 21:02:24 -0500

    edit document

--- R/Logistic.R
+++ R/Logistic.R
@@ -1,19 +1,48 @@
 #'Binomial Logistic regression
 #'
-#'Fit a generalized linear model via penalized maximum likelihood.
-#'The regularization path is computed for the lasso or elasticnet penalty
-#'at a grid of values for the regularization parameter lambda.
-#'Can deal with all shapes of data,including very large sparse data matrices.
+#'Fit a generalized linear model via unpenalized maximum likelihood.
+#'Use tuning-free optimization to get the result.
 #'
 #'@param X input matrix, of dimension n (sample number) by p (variable number);
 #' each row is an observation vector.
-#' Can be in sparse matrix format
-#' (inherit from class "sparseMatrix" as in package Matrix;
 #'@param y response variable.
-#' Since we aim at binomial distribution, it should be either a factor with two levels,
-#'  or a two-column matrix (every row is the probability of two class.)
+#' Since we aim at binomial regression, it should be a vector with two levels (like a vector of 0 & 1, or a & b)
 #'@param maxit the Maximum number of iterations when we use optimization to estimate the parameeter;
-#'  default is 5000.
+#'  default is 10000. And the algorithm will stop according to its stopping criterion.
+#'@return A list containing the relevant data of regression result.
+#'@examples
+#'
+#'### Generate training data
+#'sigma<-4
+#'set.seed(123)
+#'n<-1e4
+#'p<-1e2
+#'mu1<-rnorm(p)
+#'mu2<-rnorm(p)
+#'X1<-matrix(mu1+rnorm(n*p,0,sigma),n,p,byrow = TRUE)
+#'X2<-matrix(mu2+rnorm(n*p,0,sigma),n,p,byrow = TRUE)
+#'
+#'### Train data
+#'X<-rbind(X1,X2)
+#'y<-rep(c(1,0),each=n)
+#'
+#'### Fit model
+#'fit<-Logreg(X,y)
+#'
+#'### accuracy of train data
+#'fit$accuracy
+#'
+#'### check the convergence
+#'plot(fit$loss)
+#'
+#'### get the prediction of train data
+#'result<-fit$prediction
+#'
+#'### get the probability of each sample belonging to two category
+#'result<-fit$P
+#'
+#'### get the parameter of the logistic regression (including intercept)
+#'result<-fit$x
 #'
 #'@export
 #'   
@@ -39,7 +68,7 @@ Logreg<-function(X,y,maxit = 10000){
   pred<-rep(output[2],n)
   pred[which(X%*%result$x > 0)]<- output[1]
   result$prediction <- pred
-  result$accuracy <-   max(mean(result$prediction == yy),1-mean(result$prediction == yy)) 
+  result$accuracy <-  mean(result$prediction == y)
   result$label <-output
   return(result)
 }
